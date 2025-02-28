81fab867a7801cb470a288c94a81d99a0b7cf4ee
meebox <meebox@gmail.com>
2025-02-28 19:15:43 +0800

    modify project structure for uploading to PyPi

--- src/wrap_print/__init__.py
+++ src/wrap_print/__init__.py
@@ -19,6 +19,10 @@ class WrapPrint:
                 if ch == '\n':
                     self.pos = 0
                     continue
+                # unicodedata.east_asian_width returns:
+                # 'W': Wide(CJK characters, '你') 
+                # 'F': Fullwidth('Ａ')
+                # 'Na': Narrow width('A')
                 self.pos = self.pos + 1 + (1 if unicodedata.east_asian_width(ch) in 'WF' else 0)
                 if self.pos >= self.width:
                     print('')
@@ -26,3 +30,18 @@ class WrapPrint:
         print(end, end='')
         if '\n' in end:
             self.pos = 0                  
+
+def main():
+    wp = WrapPrint(20)
+    wp.print('12345678901234567890')
+    wp.print('This module provides access to', end='')
+    wp.print(' the Unicode Character Database (UCD)', end='')
+    wp.print(' which defines character properties', end='')
+    wp.print(' for all Unicode characters. The data contained', end='')
+    wp.print(' in this database is compiled from the UCD version 15.1.0.')
+    wp.print('這個模組提供了對 Unicode 字元資料庫（UCD）的存取，', end='')
+    wp.print('該資料庫定義了所有 Unicode 字元的字元屬性。', end='')
+    wp.print('這個資料庫中的資料是從 UCD 15.1.0 版本編譯而來的。')
+
+if __name__ == '__main__':
+    main()
